c1639f0f42caed185c2d0c180da57b7172bf263c
n.rostovtsev <n.rostovtsev@funflow.ru>
2026-05-06 16:28:09 +0300

    Fix sofusion.online

--- apps/readest-app/src/services/sofusion/api.ts
+++ apps/readest-app/src/services/sofusion/api.ts
@@ -2,7 +2,7 @@ const getBaseUrl = (): string => {
   const url = process.env['NEXT_PUBLIC_SOFUSION_API_URL'];
   console.log('[Sofusion] NEXT_PUBLIC_SOFUSION_API_URL:', url);
   if (!url) {
-    return 'https://sofusion.online/';
+    return 'https://sofusion.online';
     // throw new Error('NEXT_PUBLIC_SOFUSION_API_URL is not configured');
   }
   return url.replace(/\/+$/, '');
